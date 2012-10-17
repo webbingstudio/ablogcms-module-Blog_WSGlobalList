853f90de5e700ea11cf3e85644f2e3b84fff6bf7
WebbingStudio <mail@webbingstudio.com>
2012-10-17 16:11:16 +0900

    前にブログで公開していたのをGitHubにプッシュ

---
+++ php/ACMS/user/GET/Blog/GlobalList.php
@@ -0,0 +1,41 @@
+<?php
+
+// /php/ACMS/User/GET/Blog/GlobalList.php
+
+require_once ACMS_LIB_DIR.'GET.php';
+
+class ACMS_GET_Blog_GlobalList extends ACMS_GET
+{
+
+	function get()
+	{
+
+$DB = DB::singleton(dsn());
+$q = "SELECT * FROM acms_blog";
+
+// ブログ情報を配列で取得
+$blogArr = $DB->query($q, 'all');
+
+foreach ( $blogArr as $var ) {
+	$output[] = array(
+		'domain' => $var["blog_domain"],
+		'name'   => $var["blog_name"],
+		'id'     => $var["blog_id"],
+	);
+}
+
+// Templateクラス
+$Tpl = new Template($this->tpl, new ACMS_Corrector());
+
+// ブログ情報をループに投入
+foreach ( $output as $var ) {
+	$Tpl->add('blog:loop', $var);
+}
+
+// テンプレートを処理済みの文字列としてreturn
+return $Tpl->get();
+
+    }
+}
+
+?>
